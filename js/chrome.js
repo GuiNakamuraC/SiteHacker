33bc69d63fe6128abd0846bb089e941e2c7d072a
GuiNakamuraC <guincarvalho08@gmail.com>
2024-11-30 19:01:22 -0300

    quasefinal

---
+++ js/chrome.js
@@ -0,0 +1,15 @@
+// Função para redirecionar para inicial.html
+function goToInitialPage() {
+    window.location.href = 'inicial.html';  // Redireciona para a página inicial
+}
+
+// Função para redirecionar para senha.html
+function goToSenhaPage() {
+    window.location.href = 'senha.html';  // Redireciona para a página de senha
+}
+
+// Função para exibir uma mensagem
+function showMessage(message) {
+    // Cria um alerta personalizado
+    alert(message);
+}
